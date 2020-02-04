bd5a819fed0d7c4b7c7c1fae20672fa75d530983
EinarV77 <einar.vallejo77@gmail.com>
2020-02-04 16:13:37 +0000

    commit

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ var name = "Einar";
 var favouriteColour = "Green";
 var favouriteFood = "Mexican food";
 var city = "Tromso"
+//console prints out string variables
 console.log(name);
 console.log(favouriteColour);
 console.log(favouriteFood);
 console.log(city);
+var phrase = ' "A rainbow is formed by the sunlight. Shinning through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!" ';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+console.log(phrase+one+two+three+four+five+six+seven+endingPhrase);
