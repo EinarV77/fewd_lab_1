58b50950ef0da204dbbdfcef801532df431be26e
EinarV77 <einar.vallejo77@gmail.com>
2020-02-04 15:04:25 +0000

    commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var name = "Einar";
+var favouriteColour = "Green";
+var favouriteFood = "Mexican food";
+var city = "Tromso"
+console.log(name);
+console.log(favouriteColour);
+console.log(favouriteFood);
+console.log(city);
